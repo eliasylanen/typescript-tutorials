eb6f1de9cae7cec808b37c7d78954637494fe5b8
Elias Ylänen <elias.ylanen@gmail.com>
2017-12-28 17:03:20 +0200

    Initial commit

--- src/collectionInference.ts
+++
@@ -1,9 +0,0 @@
-const somethingAndNothing = [99, 100];
-somethingAndNothing.push(101);
-// undefined and null are subtypes of every primitive type, thus allowing this
-somethingAndNothing.push(null);
-somethingAndNothing.push(undefined);
-
-const namesAndNumbers = ['Adonis', 'Aphrodite', 12];
-// Can't add boolean to Array<string | number>
-namesAndNumbers.push(true);
